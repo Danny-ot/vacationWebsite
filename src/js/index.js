a7f836d9b98d88233f3d0176b1e62cef8095caf7
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-04-05 05:03:45 -0700

    Write the click event for the responsive navbar

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,14 @@ import '../css/styles.css';
 import {Vacation , Account , BookVacation} from './booking';
 
 
+// Responsive Navbar
+let menu = document.getElementById('menu');
+let navbar = document.getElementById('nav');
+menu.onclick = function(){
+     menu.classList.toggle("active");
+     navbar.classList.toggle("d-flex");
+};
 
+
+
+});
