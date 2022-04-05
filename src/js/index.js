0924314c7d4fbcf2a45e62398284d659a9da208b
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-04-05 05:05:44 -0700

    Create the submit event for the login form

--- src/js/index.js
+++ src/js/index.js
@@ -36,5 +36,32 @@ $(".sign-in-btn").click(()=>{
     $("#log-in").toggle();
 });
 
+// SignUp Form Submit Event
+$("#signup-form").submit((e)=>{
+    e.preventDefault();
+    let userName = $("#signup-username").val();
+    let email = $("#signup-email").val();
+    let passWord = $("#signup-password").val();
+    let confirmPassword = $("#confirm-password").val();
+    if(userName === "" || email === "" || passWord === "" || confirmPassword === ""){
+        $("#signup-error").text("Input All Fields");
+    }else{
+        if(passWord !== confirmPassword){
+            $("#signup-error").text("Passwords are no match");
+        }else{
+            $("#signup-error").text("");
+            $("#signup-username").val("");
+            $("#signup-email").val("");
+            $("#signup-password").val("");
+            $("#confirm-password").val("");
+
+            let account = new Account(userName , email , passWord);
+            vacation.addAccount(account);
+            console.log(vacation);
+        }
+    }
+});
+
+
 
 });
