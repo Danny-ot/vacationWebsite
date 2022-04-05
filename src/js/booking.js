79e9abd8550d19dc72590c8156ecdeaf18ebcc78
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-04-05 04:52:47 -0700

    Create the account constructor

--- src/js/booking.js
+++ src/js/booking.js
@@ -21,3 +21,16 @@ export class Vacation{
      }
 }
 
+// Account Class
+export class Account{
+    constructor(username , email , password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.bookings = {
+
+         };
+         this.bookingId = 0;
+         this.lastBooking = "";
+    }
+ 
